94ad89c8106334787cc1d0869a28e6079a5bca9c
Oky Mikhael <okymikhael@gmail.com>
2021-06-10 19:21:46 +0700

    Integration with home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\Gallery;
+use App\Models\Landing;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		// return view('welcome_message');
-		return view('home');
+		$gallery = new Gallery();
+		$landing = new Landing();
+        $record['data_landing'] = $landing->getLanding(1)->getRow();
+        $record['data_gallery'] = $gallery->getGallery();
+		// var_dump($record);
+		echo view('home', $record);
 	}
 }
